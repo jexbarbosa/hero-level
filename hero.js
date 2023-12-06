d86e4c3106c18c048d6a561102483e8f0bfbf0b4
Eduardo Barbosa <101431315+jexbarbosa@users.noreply.github.com>
2023-12-06 19:56:41 -0300

    Update hero.js

--- hero.js
+++ hero.js
@@ -1,22 +1,23 @@
-const nome = "DaiShi"
+let nome = "UltramanKing";
+let xp = 10000;
+let nivel = "";
 
-let xp = 7250
-
-if (xp < 1000) {
+if (xp <= 1000) {
     nivel = "Ferro"
-} else if (xp > 1001 && xp <= 2000) {
+  } else if (xp <= 2000) {
     nivel = "Bronze"
-} else if (xp > 2001 && xp <= 5000) {
+  } else if (xp <= 6000) {
     nivel = "Prata"
-} else if (xp > 6001 && xp <= 7000) {
+  } else if (xp <= 7000) {
     nivel = "Ouro"
-} else if (xp > 7001 && xp <= 8000) {
+  } else if (xp <= 8000) {
     nivel = "Platina"
-} else if (xp > 8001 && xp <= 9000) {
+  } else if (xp <= 9000) {
     nivel = "Ascendente"
-} else if (xp > 9001 && xp <= 10000) {
+  } else if (xp <= 10000) {
     nivel = "Imortal"
-} else (xp >= 10001) 
+  } else if (xp >= 10001) {
     nivel = "Radiante"
+  }
 
 console.log(`O Herói de nome ${nome} está no nível: ${nivel}`);
